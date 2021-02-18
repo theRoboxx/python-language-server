0db936a6a19699922084c45a3f69848097758ca8
krassowski <krassowski.michal@gmail.com>
2021-02-18 02:58:40 +0000

    Fix CI-only test

--- test/plugins/test_completion.py
+++ test/plugins/test_completion.py
@@ -350,7 +350,7 @@ foo.s"""
     assert completions[0]['label'] == 'spam()'
 
 
-@pytest.mark.skipif(PY2 or not LINUX or not CI, reason="tested on linux and python 3 only")
+@pytest.mark.skipif(not LINUX or not CI, reason="tested on linux and CI")
 def test_jedi_completion_environment(workspace):
     # Content of doc to test completion
     doc_content = '''import logh
@@ -377,7 +377,7 @@ def test_jedi_completion_environment(workspace):
     assert completions[0]['label'] == 'loghub'
 
     resolved = pyls_jedi_completion_item_resolve(completions[0])
-    assert 'changelog generator' in resolved['documentation'].lower()
+    assert 'changelog generator' in resolved['documentation']['value'].lower()
 
 
 def test_document_path_completions(tmpdir, workspace_other_root_path):
